9e316ee4ddb60b8be81548f86506805e497079de
im-dazai <119519606+im-dazai@users.noreply.github.com>
2022-12-29 22:38:21 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Prime_Number_Checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderEdges borderEdges1 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderEdges();
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties1 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties2 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties3 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties4 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
+            this.btnChk = new Bunifu.UI.WinForms.BunifuButton.BunifuButton();
+            this.bunifuLabel1 = new Bunifu.UI.WinForms.BunifuLabel();
+            this.txtBox = new Bunifu.UI.WinForms.BunifuTextBox();
+            this.SuspendLayout();
+            // 
+            // btnChk
+            // 
+            this.btnChk.AllowAnimations = true;
+            this.btnChk.AllowMouseEffects = true;
+            this.btnChk.AllowToggling = false;
+            this.btnChk.AnimationSpeed = 200;
+            this.btnChk.AutoGenerateColors = false;
+            this.btnChk.AutoRoundBorders = false;
+            this.btnChk.AutoSizeLeftIcon = true;
+            this.btnChk.AutoSizeRightIcon = true;
+            this.btnChk.BackColor = System.Drawing.Color.Transparent;
+            this.btnChk.BackColor1 = System.Drawing.Color.DodgerBlue;
+            this.btnChk.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnChk.BackgroundImage")));
+            this.btnChk.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btnChk.ButtonText = "Check";
+            this.btnChk.ButtonTextMarginLeft = 0;
+            this.btnChk.ColorContrastOnClick = 45;
+            this.btnChk.ColorContrastOnHover = 45;
+            this.btnChk.Cursor = System.Windows.Forms.Cursors.Default;
+            borderEdges1.BottomLeft = true;
+            borderEdges1.BottomRight = true;
+            borderEdges1.TopLeft = true;
+            borderEdges1.TopRight = true;
+            this.btnChk.CustomizableEdges = borderEdges1;
+            this.btnChk.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnChk.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.btnChk.DisabledFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.btnChk.DisabledForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(160)))), ((int)(((byte)(168)))));
+            this.btnChk.FocusState = Bunifu.UI.WinForms.BunifuButton.BunifuButton.ButtonStates.Pressed;
+            this.btnChk.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnChk.ForeColor = System.Drawing.Color.White;
+            this.btnChk.IconLeftAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnChk.IconLeftCursor = System.Windows.Forms.Cursors.Default;
+            this.btnChk.IconLeftPadding = new System.Windows.Forms.Padding(11, 3, 3, 3);
+            this.btnChk.IconMarginLeft = 11;
+            this.btnChk.IconPadding = 10;
+            this.btnChk.IconRightAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnChk.IconRightCursor = System.Windows.Forms.Cursors.Default;
+            this.btnChk.IconRightPadding = new System.Windows.Forms.Padding(3, 3, 7, 3);
+            this.btnChk.IconSize = 25;
+            this.btnChk.IdleBorderColor = System.Drawing.Color.DodgerBlue;
+            this.btnChk.IdleBorderRadius = 25;
+            this.btnChk.IdleBorderThickness = 1;
+            this.btnChk.IdleFillColor = System.Drawing.Color.DodgerBlue;
+            this.btnChk.IdleIconLeftImage = null;
+            this.btnChk.IdleIconRightImage = null;
+            this.btnChk.IndicateFocus = false;
+            this.btnChk.Location = new System.Drawing.Point(79, 198);
+            this.btnChk.Name = "btnChk";
+            this.btnChk.OnDisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.btnChk.OnDisabledState.BorderRadius = 25;
+            this.btnChk.OnDisabledState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btnChk.OnDisabledState.BorderThickness = 1;
+            this.btnChk.OnDisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.btnChk.OnDisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(160)))), ((int)(((byte)(168)))));
+            this.btnChk.OnDisabledState.IconLeftImage = null;
+            this.btnChk.OnDisabledState.IconRightImage = null;
+            this.btnChk.onHoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+            this.btnChk.onHoverState.BorderRadius = 25;
+            this.btnChk.onHoverState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btnChk.onHoverState.BorderThickness = 1;
+            this.btnChk.onHoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+            this.btnChk.onHoverState.ForeColor = System.Drawing.Color.White;
+            this.btnChk.onHoverState.IconLeftImage = null;
+            this.btnChk.onHoverState.IconRightImage = null;
+            this.btnChk.OnIdleState.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.btnChk.OnIdleState.BorderRadius = 25;
+            this.btnChk.OnIdleState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btnChk.OnIdleState.BorderThickness = 1;
+            this.btnChk.OnIdleState.FillColor = System.Drawing.Color.DodgerBlue;
+            this.btnChk.OnIdleState.ForeColor = System.Drawing.Color.White;
+            this.btnChk.OnIdleState.IconLeftImage = null;
+            this.btnChk.OnIdleState.IconRightImage = null;
+            this.btnChk.OnPressedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(96)))), ((int)(((byte)(144)))));
+            this.btnChk.OnPressedState.BorderRadius = 25;
+            this.btnChk.OnPressedState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btnChk.OnPressedState.BorderThickness = 1;
+            this.btnChk.OnPressedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(96)))), ((int)(((byte)(144)))));
+            this.btnChk.OnPressedState.ForeColor = System.Drawing.Color.White;
+            this.btnChk.OnPressedState.IconLeftImage = null;
+            this.btnChk.OnPressedState.IconRightImage = null;
+            this.btnChk.Size = new System.Drawing.Size(170, 66);
+            this.btnChk.TabIndex = 0;
+            this.btnChk.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnChk.TextAlignment = System.Windows.Forms.HorizontalAlignment.Center;
+            this.btnChk.TextMarginLeft = 0;
+            this.btnChk.TextPadding = new System.Windows.Forms.Padding(0);
+            this.btnChk.UseDefaultRadiusAndThickness = true;
+            this.btnChk.Click += new System.EventHandler(this.btnChk_Click);
+            // 
+            // bunifuLabel1
+            // 
+            this.bunifuLabel1.AllowParentOverrides = false;
+            this.bunifuLabel1.AutoEllipsis = false;
+            this.bunifuLabel1.AutoSize = false;
+            this.bunifuLabel1.CursorType = System.Windows.Forms.Cursors.Default;
+            this.bunifuLabel1.Font = new System.Drawing.Font("Pancho", 9.75F);
+            this.bunifuLabel1.Location = new System.Drawing.Point(79, 39);
+            this.bunifuLabel1.Name = "bunifuLabel1";
+            this.bunifuLabel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.bunifuLabel1.Size = new System.Drawing.Size(215, 19);
+            this.bunifuLabel1.TabIndex = 2;
+            this.bunifuLabel1.Text = "Prime or Composite Checker";
+            this.bunifuLabel1.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.bunifuLabel1.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // txtBox
+            // 
+            this.txtBox.AcceptsReturn = false;
+            this.txtBox.AcceptsTab = false;
+            this.txtBox.AnimationSpeed = 200;
+            this.txtBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.txtBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
+            this.txtBox.BackColor = System.Drawing.Color.White;
+            this.txtBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("txtBox.BackgroundImage")));
+            this.txtBox.BorderColorActive = System.Drawing.Color.DodgerBlue;
+            this.txtBox.BorderColorDisabled = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.txtBox.BorderColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+            this.txtBox.BorderColorIdle = System.Drawing.Color.Silver;
+            this.txtBox.BorderRadius = 1;
+            this.txtBox.BorderThickness = 1;
+            this.txtBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.txtBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtBox.DefaultFont = new System.Drawing.Font("Segoe UI", 9.25F);
+            this.txtBox.DefaultText = "";
+            this.txtBox.FillColor = System.Drawing.Color.White;
+            this.txtBox.HideSelection = true;
+            this.txtBox.IconLeft = null;
+            this.txtBox.IconLeftCursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtBox.IconPadding = 10;
+            this.txtBox.IconRight = null;
+            this.txtBox.IconRightCursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtBox.Lines = new string[0];
+            this.txtBox.Location = new System.Drawing.Point(41, 97);
+            this.txtBox.MaxLength = 32767;
+            this.txtBox.MinimumSize = new System.Drawing.Size(1, 1);
+            this.txtBox.Modified = false;
+            this.txtBox.Multiline = false;
+            this.txtBox.Name = "txtBox";
+            stateProperties1.BorderColor = System.Drawing.Color.DodgerBlue;
+            stateProperties1.FillColor = System.Drawing.Color.Empty;
+            stateProperties1.ForeColor = System.Drawing.Color.Empty;
+            stateProperties1.PlaceholderForeColor = System.Drawing.Color.Empty;
+            this.txtBox.OnActiveState = stateProperties1;
+            stateProperties2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            stateProperties2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            stateProperties2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            stateProperties2.PlaceholderForeColor = System.Drawing.Color.DarkGray;
+            this.txtBox.OnDisabledState = stateProperties2;
+            stateProperties3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+            stateProperties3.FillColor = System.Drawing.Color.Empty;
+            stateProperties3.ForeColor = System.Drawing.Color.Empty;
+            stateProperties3.PlaceholderForeColor = System.Drawing.Color.Empty;
+            this.txtBox.OnHoverState = stateProperties3;
+            stateProperties4.BorderColor = System.Drawing.Color.Silver;
+            stateProperties4.FillColor = System.Drawing.Color.White;
+            stateProperties4.ForeColor = System.Drawing.Color.Empty;
+            stateProperties4.PlaceholderForeColor = System.Drawing.Color.Empty;
+            this.txtBox.OnIdleState = stateProperties4;
+            this.txtBox.Padding = new System.Windows.Forms.Padding(3);
+            this.txtBox.PasswordChar = '\0';
+            this.txtBox.PlaceholderForeColor = System.Drawing.Color.Silver;
+            this.txtBox.PlaceholderText = "Enter Number";
+            this.txtBox.ReadOnly = false;
+            this.txtBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtBox.SelectedText = "";
+            this.txtBox.SelectionLength = 0;
+            this.txtBox.SelectionStart = 0;
+            this.txtBox.ShortcutsEnabled = true;
+            this.txtBox.Size = new System.Drawing.Size(253, 37);
+            this.txtBox.Style = Bunifu.UI.WinForms.BunifuTextBox._Style.Bunifu;
+            this.txtBox.TabIndex = 1;
+            this.txtBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtBox.TextMarginBottom = 0;
+            this.txtBox.TextMarginLeft = 3;
+            this.txtBox.TextMarginTop = 0;
+            this.txtBox.TextPlaceholder = "Enter Number";
+            this.txtBox.UseSystemPasswordChar = false;
+            this.txtBox.WordWrap = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 451);
+            this.Controls.Add(this.bunifuLabel1);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.btnChk);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.UI.WinForms.BunifuButton.BunifuButton btnChk;
+        private Bunifu.UI.WinForms.BunifuLabel bunifuLabel1;
+        private Bunifu.UI.WinForms.BunifuTextBox txtBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prime_Number_Checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChk_Click(object sender, EventArgs e)
+        {
+           
+
+            int n, i, Chk;
+
+              n = Convert.ToInt32(txtBox.Text);
+
+                Chk = 0;
+                i = 2;
+                while (i <= n / 2)
+                {
+                    if (n % i == 0)
+                    {
+                        Chk = 1;
+                        break;
+                    }
+                    i++;
+                }
+
+           if (Chk == 0)
+                MessageBox.Show(n + " is a Prime Number.");
+            
+            else
+                MessageBox.Show(n + " is a Composite Number.");
+
+        }
+    }
+    }
